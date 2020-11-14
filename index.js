99a5baa4b8c8bbe1d2fa56e46a22ed2239fc2fe9
SS Jones <SSJones.code@gmail.com>
2020-11-14 13:33:27 -0700

    I created a class constructor to create an object literal that contains a function and then after exporting said class and importing it to a new file, I then created an instance of the class. Then, with the dot operator, I ran the function created in conjunction with the instantiated class to produce an object that runs a the funciton, thus having a person say a greeting.

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 //require imports the file that is named in the ()
 //the ./ is because person is a file and not a module that was installed or a core module
-const person = require('./person');
+const Person = require('./person');
 
-console.log(person);
-console.log(person.name);
-console.log(person.age);
+//first I did this:
+// console.log(person);
+// console.log(person.name);
+// console.log(person.age);
 
+//next I did this
+const person1 = new Person('John Doe', 30);
+person1.greeting();
--- person.js
+++ person.js
@@ -1,7 +1,18 @@
-const person = {
-    name: 'John Doe',
-    age: 57
-} 
+// const person = {
+//     name: 'John Doe',
+//     age: 57
+// } 
+
+//I created a class constructor of Person; so marked by the capital P in the word Person, to make a Person object literal and used the this keyword to specify what name and age are in that particular constructor. I placed a greeting function in the constructor as well.
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    greeting() {
+        console.log(`My name is ${this.name} and I am ${this.age}`)
+    }
+}
 
 //below is how an item is exported
-module.exports = person;
+module.exports = Person;
