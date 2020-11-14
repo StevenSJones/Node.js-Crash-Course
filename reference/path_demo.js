d69cb5b6a677c39002f57d603730121ccb31cc7e
SS Jones <SSJones.code@gmail.com>
2020-11-14 15:54:02 -0700

    Using path.extname and passing in the filename method provided by the module wrapper function to return the extension name.

--- reference/path_demo.js
+++ reference/path_demo.js
@@ -7,7 +7,7 @@ console.log(__filename);
 // /Users/stevenjones/Practice-post-BC/Node.js-Crash-Course/reference/path_demo.js
 
 /*---------------------------------------------------------*/
-
+//Base file name
 //using path.basename() and passing in the filename method provided by the module wrapper function to return the dir and file names
 console.log(path.basename(__filename));
 //produces:
@@ -15,7 +15,7 @@ console.log(path.basename(__filename));
 // path_demo.js
 
 /*---------------------------------------------------------*/
-
+//Directory name
 //Using path.dirname() and passing in the filename method provided by the module wrapper function to return the dir name
 console.log(path.dirname(__filename));
 //produces:
@@ -23,3 +23,15 @@ console.log(path.dirname(__filename));
 // path_demo.js
 // /Users/stevenjones/Practice-post-BC/Node.js-Crash-Course/reference
 
+/*---------------------------------------------------------*/
+//File extension
+console.log(path.extname(__filename));
+//produces:
+// /Users/stevenjones/Practice-post-BC/Node.js-Crash-Course/reference/path_demo.js
+// path_demo.js
+// /Users/stevenjones/Practice-post-BC/Node.js-Crash-Course/reference
+// .js
+
+/*---------------------------------------------------------*/
+//Create path object
+console.log(path.parse(__filename));
