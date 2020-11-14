5e1e7e51761266e7371dc82b013b2cda8bcc0fec
SS Jones <SSJones.code@gmail.com>
2020-11-14 13:49:34 -0700

    I included more substantial comments for better understanding when looking back.

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-//require imports the file that is named in the ()
+//This require is written in common js. require imports the file that is named in the ()
+
 //the ./ is because person is a file and not a module that was installed or a core module
 const Person = require('./person');
 
-//first I did this:
+
+//first: I console.logged; using the dot operator, the person itself, as well as the name and age of the person.
 // console.log(person);
 // console.log(person.name);
 // console.log(person.age);
 
-//next I did this
+//Second: I created a class constructor to create an object literal that contains a function and then after exporting said class and importing it to a new file, I then created an instance of the class. Then, with the dot operator, I ran the function created in conjunction with the instantiated class to produce an object that runs a the funciton, thus having a person say a greeting.'
 const person1 = new Person('John Doe', 30);
 person1.greeting();
+
+
