ce98975d80dd25b94d38adb13c18105ae0cba4d5
SS Jones <SSJones.code@gmail.com>
2020-11-14 14:34:06 -0700

    I used the filename method provided by the module wrapper function.

---
+++ reference/path_demo.js
@@ -0,0 +1,7 @@
+const path = require('path');
+//Above: path is a core module so no need to include ./ (filepath or run npm install path
+
+//file name
+console.log(__filename);
+//produces:
+// /Users/stevenjones/Practice-post-BC/Node.js-Crash-Course/reference/path_demo.js
