359837fd8a86d93d44d9a5574416bfbc898784f1
SS Jones <SSJones.code@gmail.com>
2020-11-14 14:40:50 -0700

    Using path.basename and passing in the filename method provided by the module wrapper function. The module wrapper function is automatically included before a modules code is executed. In this instance, the module being the path module.

--- reference/path_demo.js
+++ reference/path_demo.js
@@ -5,3 +5,13 @@ const path = require('path');
 console.log(__filename);
 //produces:
 // /Users/stevenjones/Practice-post-BC/Node.js-Crash-Course/reference/path_demo.js
+
+/*---------------------------------------------------------*/
+
+//using path.basename and passing in the filename method provided by the module wrapper function.
+console.log(path.basename(__filename));
+//produces the:
+// /Users/stevenjones/Practice-post-BC/Node.js-Crash-Course/reference/path_demo.js
+path_demo.js
+
+/*---------------------------------------------------------*/
