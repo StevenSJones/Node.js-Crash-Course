d07d53d460c4fe1a5c3c2fab09aa059c1a6b7ee5
SS Jones <SSJones.code@gmail.com>
2020-11-17 20:02:48 -0700

    I created the file called hello.txt inside of the dir that I just previously created called test AND simultaneously added text to that file.

--- reference/fs_demo.js
+++ reference/fs_demo.js
@@ -8,7 +8,15 @@ const path = require("path");
 //     console.log('Folder created...');
 // });
 //below is the mkdir function with an arrow function. With the arrow function, if there is only one parameter then we do not need the parentheses around err. If there were multiple parameters then we would.
-fs.mkdir(path.join(__dirname, "/test"), {}, (err) => {
-  if (err) throw err;
-  console.log("Folder Created...");
-});
+// fs.mkdir(path.join(__dirname, "/test"), {}, err => {
+//   if (err) throw err;
+//   console.log("Folder Created...");
+// });
+//this produces a new folder (dir) called test.
+
+//create and write to a file
+// fs.writeFile(path.join(__dirname, '/test', 'hello.txt'), 'Hello World!', err => {
+//     if (err) throw err;
+//     console.log("File written to...");
+//   });
+  //this produces a new file inside of test called hello.txt AND added the text 'Hello World' to it simultaneously.
