68d960ee0fa3c49ad35f2764cca91ed33fe6062b
SS Jones <SSJones.code@gmail.com>
2020-11-18 14:06:05 -0700

    I ran os.platform to show in the terminal what operating system platform I am running on, which is darwin for a MAC.

--- reference/os_demo.js
+++ reference/os_demo.js
@@ -2,3 +2,5 @@ const os = require('os');
 //above I imported the Operating System module. It is a core module so no need to include ./ (filepath) or run npm install path
 
 // Platform 
+console.log(os.platform());
+// Produces darwin in the terminal as that is the operating system for Mac. It would be win32 for Windows and something else for linux
