b3a392a7846f72f8b88d9735b77ee152602e73a9
SS Jones <SSJones.code@gmail.com>
2020-11-18 14:35:15 -0700

    I ran the href property on the object that I instantiated after requiring the url module AND THEN I ran the same object with the toString() method and it returns the same thing which is the entire url that i used in the myURL instantiation.

--- reference/url_demo.js
+++ reference/url_demo.js
@@ -1,3 +1,8 @@
 const url = require('url');
 // I imported the url module and then below I instantiated a new URL object. This must be done for the import to work. 
 const myURL = new URL ('http://mywebsite.com/hello.html?id=100&status=active');
+
+// Serialized URL using the href property NOT A METHOD
+console.log(myURL.href);
+//using the toString method produces the same return
+console.log(myURL.toString());
