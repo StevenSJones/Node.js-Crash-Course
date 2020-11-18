35ecfe77bc93d0db8053995f13957c5b55afa473
SS Jones <SSJones.code@gmail.com>
2020-11-17 19:54:03 -0700

    I used the mkdir method and a path.join to create a folder; otherwise known as a directory, in the dir that I am in. The dir I am in happens to be reference.

--- reference/fs_demo.js
+++ reference/fs_demo.js
@@ -1,9 +1,14 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
-//create folder, BUT THERE IS A SYCRONOUS VERSION. This version, the asyncronous verion, takes a callback. 
+//create folder, BUT THERE IS A SYCRONOUS VERSION. This version, the asyncronous verion, takes a callback.
 //Below is the mkdir function with the standard function syntax.
 // fs.mkdir(path.join(__dirname, '/test'), {}, function (err) {
 //     if(err) throw err;
 //     console.log('Folder created...');
 // });
+//below is the mkdir function with an arrow function. With the arrow function, if there is only one parameter then we do not need the parentheses around err. If there were multiple parameters then we would.
+fs.mkdir(path.join(__dirname, "/test"), {}, (err) => {
+  if (err) throw err;
+  console.log("Folder Created...");
+});
