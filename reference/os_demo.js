ec5d8cd1a48fb9982c91912e0df92b6b30a6e3cb
SS Jones <SSJones.code@gmail.com>
2020-11-18 14:10:10 -0700

    I ran os.arch() which is an inbuilt application programming interface of the os module which is used to get CPU architecture of the computer for which the current node.js is compiled. Returns x64.

--- reference/os_demo.js
+++ reference/os_demo.js
@@ -4,3 +4,8 @@ const os = require('os');
 // Platform 
 console.log(os.platform());
 // Produces darwin in the terminal as that is the operating system for Mac. It would be win32 for Windows and something else for linux
+
+// CPU Arch
+console.log(os.arch());
+// os.arch() method is an inbuilt application programming interface of the os module which is used to get CPU architecture of the computer for which the current node.js is compiled
+// produces 
