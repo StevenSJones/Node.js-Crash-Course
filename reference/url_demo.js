af6dd20a624bed0c4722a97f27db62b0da5ffbe7
SS Jones <SSJones.code@gmail.com>
2020-11-18 14:30:12 -0700

    I imported the url module and then below I instantiated a new URL object. This must be done for the import to work.

---
+++ reference/url_demo.js
@@ -0,0 +1,3 @@
+const url = require('url');
+// I imported the url module and then below I instantiated a new URL object. This must be done for the import to work. 
+const myURL = new URL ('http://mywebsite.com/hello.html?id=100&status=active');
