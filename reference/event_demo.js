9c8a60c5b7683c100a0282b643acf37ed353930f
SS Jones <SSJones.code@gmail.com>
2020-11-18 15:11:04 -0700

    importing the events module, then creating a class constructor that extends the import above. Create an init object using the constructor, then create an event listener on the new object that says (in the console) Event Fired upon the firing of the event and finally the event that will trigger the listener itself was created.

---
+++ reference/event_demo.js
@@ -0,0 +1,13 @@
+const EventEmitter = require('events');
+//importing the events module
+// then creating a class constructor that extends the import above
+class MyEmitter extends EventEmitter { }
+
+//create an init object using the constructor
+const myEmitter = new MyEmitter();
+
+//event listener on the new object that says (in the console) 'Event Fired' upon the firing of the event.
+myEmitter.on('event', () => console.log('Event Fired!'))
+
+//init event that will trigger the listener that is listening for this exact event.
+myEmitter.emit('event');
