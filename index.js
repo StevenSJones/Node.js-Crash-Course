94f48636edbf9703d2b7a7b88ca1888dc4b42eaa
SS Jones <SSJones.code@gmail.com>
2020-11-20 14:32:28 -0700

    I included the success route or file path with a writeHead() and a res.end(). The res.end serves up the content in the event that there are no errors.

--- index.js
+++ index.js
@@ -58,20 +58,26 @@ const server = http.createServer((req, res) => {
   }
 
   fs.readFile(filePath, (err, content) => {
-      if(err) {
-          if(err.code === 'ENOENT') {
-              //Page not found
-              fs.readFile(path.join(__dirname, 'public', '404.html'), (err, content) => {
-                    res.writeHead(200, { "Content-Type": "text/html" }); //write the headers
-                    res.end(content, 'utf8');
-              })
-          } else {
-              // Some other server error
-              res.writeHead(500);
-              res.end(`Server Error: ${err.code}`);
+    if (err) {
+      if (err.code === "ENOENT") {
+        //Page not found
+        fs.readFile(
+          path.join(__dirname, "public", "404.html"),
+          (err, content) => {
+            res.writeHead(200, { "Content-Type": "text/html" }); //write the headers
+            res.end(content, "utf8");
           }
+        );
+      } else {
+        // Some other server error
+        res.writeHead(500);
+        res.end(`Server Error: ${err.code}`);
       }
-
+    } else {
+      // Success
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(content, "utf8");
+    }
   });
 });
 
